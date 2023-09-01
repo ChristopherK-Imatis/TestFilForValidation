78deae2088cd801022eb41f13adcaa8240008809
Christopher <herman.kittilsen@DNVimatis.com>
2023-09-01 13:04:15 +0200

    Made three create page tests

--- SimTest/UnitTest1.cs
+++ SimTest/UnitTest1.cs
@@ -1,6 +1,9 @@
 using TestFilForValidation.Data;
 using TestFilForValidation.Pages;
+using System.Text.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bunit;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace SimTest
 {    
@@ -8,22 +11,69 @@ namespace SimTest
     public class CreateTests
     {
         [TestMethod]
-        public void TestValidationLogic_EmptyFields_ShouldFail()
+        public void NextPatientAfter_5_ShouldBe_6()
         {
             //Arrange
-            var page = new Create();
+            var sut = new Create();
+            var testData = new Data();
+            testData.Employees = new List<Employee>
+            {
+                    new Employee { EmployeeID = 1 },
+                    new Employee { EmployeeID = 2 },
+                    new Employee { EmployeeID = 3 },
+                    new Employee { EmployeeID = 4 },
+                    new Employee { EmployeeID = 5 }
+            };
 
-            var consoleOut = new StringWriter();
-            Console.SetOut(consoleOut);
+            sut.data = testData;
+            sut.newEmployee= new Employee();
 
             //Act
-            page.HandleInvalidSubmit(); //Trigger the method
+            sut.SetEmployeeId();
 
             //Assert
-            Assert.IsTrue(consoleOut.ToString().Contains("Invalid submit"));
+            Assert.AreEqual(6, sut.newEmployee.EmployeeID);
+        }
 
-            //Clean up! Clean up! Clean up! Clean up!
-            consoleOut.Dispose();
+        [TestMethod]
+        public void TestValidationLogic_EmptyFields_ShouldFail()
+        {
+            using var ctx = new Bunit.TestContext();
+
+            // Redirect the console output to a StringWriter
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            var cut = ctx.RenderComponent<Create>();
+
+            // Act: Submit the form without filling in any fields
+            cut.Find("form").Submit();
+
+            // Assert: Capture and check the console output
+            var consoleOutput = sw.ToString();
+            Assert.IsTrue(consoleOutput.Contains("Invalid submit"));
+        }
+
+        [TestMethod]
+        public async Task TestIfDataIsLoadingCorrectly()
+        {
+            // Arrange
+            var jsonContent = JsonSerializer.Serialize(new Data());
+            File.WriteAllText("testdata.json", jsonContent);
+
+            using var ctx = new Bunit.TestContext();
+            var cut = ctx.RenderComponent<Create>();
+
+            // Act
+            // Call the public method to simulate component initialization
+            await cut.Instance.InitializeDataForTestingAsync();
+
+            // Assert
+            var newPatient = cut.Instance.newEmployee;
+            Assert.IsNotNull(newPatient);
+
+            // Clean up
+            File.Delete("testdata.json");
         }
     }
 }
